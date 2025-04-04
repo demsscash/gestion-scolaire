2f359a35e71ee89ea90752dd8b44c606777bbe25
demsscash <demsscash@gmail.com>
2025-04-04 18:26:02 +0000

    test

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,10 +5,10 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use App\Http\Middleware\CheckRole;
 
-
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',  // Ajoutez cette ligne
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
@@ -22,7 +22,6 @@ return Application::configure(basePath: dirname(__DIR__))
             \Illuminate\Routing\Middleware\ThrottleRequests::class . ':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ]);
-        //
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
